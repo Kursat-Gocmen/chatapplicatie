7527bf866aea48b52435eb2321553a7abcd100d5
Kursat Gocmen Göçmen <505258@student.fontys.nl>
2023-10-16 15:46:47 +0200

    upload to git

---
+++ src/main/java/com/backend/chatapplicatie/ChatapplicatieApplication.java
@@ -0,0 +1,13 @@
+package com.backend.chatapplicatie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatapplicatieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatapplicatieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/chatapplicatie/ChatapplicatieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.chatapplicatie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatapplicatieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
