8b7df5f522424485af0c4a138cee08ce1b40194e
Kursat-Gocmen <ferhatrxls@gmail.com>
2023-12-12 21:32:04 +0100

    Added Integration and JUNIT tests and made changes for CI/CD

--- src/test/java/com/backend/chatapplicatie/services/UserServiceTest.java
+++ src/test/java/com/backend/chatapplicatie/services/UserServiceTest.java
@@ -7,12 +7,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 import java.util.Arrays;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
 public class UserServiceTest {
     @InjectMocks
     private UserService userService;
