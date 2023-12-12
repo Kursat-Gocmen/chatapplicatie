5174b477f84c3f1a012bcdfba8da7b0c8d8dc958
Kursat-Gocmen <ferhatrxls@gmail.com>
2023-12-12 19:06:25 +0100

    Added Integration and JUNIT tests and made changes for CI/CD

--- src/test/java/com/backend/chatapplicatie/controllers/AuthControllerIntegrationTest.java
+++ src/test/java/com/backend/chatapplicatie/controllers/AuthControllerIntegrationTest.java
@@ -3,13 +3,17 @@ package com.backend.chatapplicatie.controllers;
 import com.backend.chatapplicatie.ChatapplicatieApplication;
 import com.backend.chatapplicatie.payload.request.LoginRequest;
 import com.backend.chatapplicatie.payload.request.SignUpRequest;
+import com.backend.chatapplicatie.repository.RoleRepository;
+import com.backend.chatapplicatie.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,7 +21,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-
 public class AuthControllerIntegrationTest {
 
     @Autowired
@@ -26,6 +29,13 @@ public class AuthControllerIntegrationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private UserRepository userRepository;
+    @AfterEach
+    public void cleanup() {
+        // Clean up the user table
+        userRepository.deleteAll();
+    }
     @Test
     public void testRegisterUser() throws Exception {
 
