2ab78e4ddfd812f9333c74a02429758236bd13d3
Kursat-Gocmen <ferhatrxls@gmail.com>
2023-12-12 21:22:56 +0100

    Added Integration and JUNIT tests and made changes for CI/CD

--- src/test/java/com/backend/chatapplicatie/services/UserDetailsServiceImplTest.java
+++ src/test/java/com/backend/chatapplicatie/services/UserDetailsServiceImplTest.java
@@ -9,10 +9,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.TestPropertySource;
 
 @ExtendWith(MockitoExtension.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class UserDetailsServiceImplTest {
 
     @Mock
