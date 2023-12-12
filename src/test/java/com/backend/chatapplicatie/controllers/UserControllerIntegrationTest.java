9c86ad5649db85b98f9804d067c6e786248b42c3
Kursat-Gocmen <ferhatrxls@gmail.com>
2023-12-12 20:13:59 +0100

    Added Integration and JUNIT tests and made changes for CI/CD

--- src/test/java/com/backend/chatapplicatie/controllers/UserControllerIntegrationTest.java
+++ src/test/java/com/backend/chatapplicatie/controllers/UserControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import com.backend.chatapplicatie.services.UserService;
 import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(locations = "classpath:application-test.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserControllerIntegrationTest {
 
     @Autowired
