43ecf69e32ea587d6d081c8d6c7dce246277b511
Kursat-Gocmen <ferhatrxls@gmail.com>
2023-12-12 20:07:29 +0100

    Added Integration and JUNIT tests and made changes for CI/CD

--- src/test/java/com/backend/chatapplicatie/controllers/AuthControllerIntegrationTest.java
+++ src/test/java/com/backend/chatapplicatie/controllers/AuthControllerIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application-test.properties")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")
 public class AuthControllerIntegrationTest {
 
     @Autowired
